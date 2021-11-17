7f6d76f4ba3f6c903ddfae328dfe7a84c5a61391
Thada Wangthammang <thada.wth@gmail.com>
2021-11-17 09:20:26 +0700

    test

--- merge.sh
+++ merge.sh
@@ -2,6 +2,9 @@
 ACCESS_TOKEN=$1
 BASE_REF=$2
 HEAD_REF=$3
+echo ACCESS_TOKEN = $ACCESS_TOKEN
+echo BASE_REF = $BASE_REF
+echo HEAD_REF = $HEAD_REF
 git config --global user.email "my@wrmsoftware.com"
 git config --global user.name "Merge script"
 git clone https://mywrm:$ACCESS_TOKEN@github.com/my-wrm/dummy-project.git
